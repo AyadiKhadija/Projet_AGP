cc88645b071d37f90b0850a15401883ce2c2e1a9
billf <billf@LAPTOP-8E9QMSRD>
2022-01-17 10:05:06 +0100

    Billal : modification jdbc Connection

--- bank7/src/persistence/jdbc/JdbcConnection.java
+++ bank7/src/persistence/jdbc/JdbcConnection.java
@@ -1,24 +1,13 @@
 package persistence.jdbc;
-//First commit fatia
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 class JdbcConnection {
 	private static String host = "localhost";
-	private static String base = "bank6jdbc";
+	private static String base = "Canarie";
 	private static String user = "root";
 	private static String password = "";
 	private static String url = "jdbc:mysql://" + host + "/" + base;
-	
-	/**
-	 * Fatia connection
-	 * private static String host = "localhost:8889";
-	private static String base = "b";
-	private static String user = "root";
-	private static String password = "root";
-	private static String url = "jdbc:mysql://" + host + "/" + base;
-	 * 
-	 */
 	
 	/**
 	 * Lazy singleton instance.
